6dfe0499530e77e8e0105ac4a4f2692272263da8
shashankpasumarthi <98116079+shashank2514@users.noreply.github.com>
2022-04-22 09:52:16 -0500

    added

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require("morgan");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }))
 
 const connectionString = "mongodb+srv://sha:sha@cluster0.asknq.mongodb.net/learnMongo?retryWrites=true&w=majority";
 mongoose = require('mongoose');
@@ -39,6 +55,10 @@ app.use('/selector', selectorRouter)
 app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
